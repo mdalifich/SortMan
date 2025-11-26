d9dbfbc3873c830e5e16e738a81e4286f5aa615e
mdalifich <vasjapupkin.2020@mail.ru>
2025-11-26 11:24:47 +0300

    HistoryWindows

--- bin/HistoryWindowClass.py
+++ bin/HistoryWindowClass.py
@@ -20,7 +20,53 @@ from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QLa
     QGridLayout, QHBoxLayout, QVBoxLayout, QMenuBar, QAction, QLineEdit
 
 
-class SortMain(QWidget):
+class HistoryWind(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('bin/Windows/SortWindow.ui', self)
+        self.curs = None
+        self.DataBase = None
+        self.LikeDataBase = None
+        uic.loadUi('bin/Windows/HistoryWindow.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.SbrossButton.clicked.connect(self.sbross)
+        self.filtrButton.clicked.connect(self.filtr)
+        self.historyTexBox.setText(self.getText())
+
+    def filtr(self):
+        time = self.TimeEdit.dateTime()
+        dat = time.date()
+        tm = time.time().hour()
+        res = f'{dat.year()}-{dat.month()}-{dat.day()} {tm}'
+        print(res)
+        self.historyTexBox.setText(self.getText(comand=res))
+
+    def sbross(self):
+        self.historyTexBox.setText(self.getText())
+
+    def getText(self, comand=''):
+        res = ''
+        result = ''
+        if comand == '':
+            self.OpenDataBase()
+            self.LikeDataBase = 'SELECT * FROM Files'
+            res = self.curs.execute(self.LikeDataBase).fetchall()
+            self.CloseDataBase()
+        else:
+            self.OpenDataBase()
+            self.LikeDataBase = f'SELECT * FROM Files WHERE SortedDate = "{comand}"'
+            res = self.curs.execute(self.LikeDataBase).fetchall()
+            self.CloseDataBase()
+        for i in res:
+            result += '\n=================================\n'
+            result += f'Название: {i[1]}\n\nВходной путь: {i[3]}\n\nВыходной путь: {i[2]}\n\nДата сортировки: {i[4]} часов'
+        return result
+
+    def OpenDataBase(self):
+        self.DataBase = sqlite3.connect('bin/DataBase/BD.sqlite')
+        self.curs = self.DataBase.cursor()
+
+    def CloseDataBase(self):
+        self.DataBase.commit()
+        self.DataBase.close()
--- bin/MainWindowClass.py
+++ bin/MainWindowClass.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 import os
 import glob
@@ -13,6 +14,9 @@ import bin.ErrorWindowClass
 from bin.ErrorWindowClass import *
 
 
+from bin.HistoryWindowClass import *
+
+
 from PyQt5 import QtGui, QtCore
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal, QObject
@@ -23,8 +27,28 @@ from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QLa
 class SortMain(QWidget):
     def __init__(self):
         super().__init__()
-        with open('bin/DefaultPath.txt', mode='rt') as f:
-            self.ToPath = f.readlines()[0]
+        self.Error = None
+        self.DataBase = None
+        self.curs = None
+        self.WindFormat = None
+        self.LikeDataBase = None
+        try:
+            with open('bin/DefaultPath.txt', mode='rt') as f:
+                k = f.readlines()
+                if len(k) == 1:
+                    self.ToPath = k[0]
+                else:
+                    self.ToPath = "No directory"
+        except FileNotFoundError:
+            with open('bin/DefaultPath.txt', mode='at') as f:
+                k = f.readlines()
+                if len(k) != 0:
+                    self.ToPath = k[0]
+                else:
+                    self.ToPath = "No directory"
+        if self.ToPath == "No directory":
+            with open('bin/DefaultPath.txt', mode='wt') as f:
+                f.write('No directory')
         self.DB = None
         uic.loadUi('bin/Windows/SortWindow.ui', self)
         self.ToFile = None
@@ -32,12 +56,17 @@ class SortMain(QWidget):
         self.initUi()
 
     def initUi(self):
+        self.openHistory.clicked.connect(self.openHistoryfnc)
         self.SelectPath.clicked.connect(self.SelectP)
         self.SelectFileButton.clicked.connect(self.SelectFile)
         self.StartButton.clicked.connect(self.Sort)
         self.AllOrSelectButton.clicked.connect(self.AllOrSelect)
         self.SelectFormatButton.clicked.connect(self.CallFormat)
         self.SelectFileLineEdit_2.setText(self.ToPath)
+
+    def openHistoryfnc(self):
+        self.HistoryClass = HistoryWind()
+        self.HistoryClass.show()
 
     def SelectP(self):
         self.ToFile = QFileDialog.getExistingDirectory(self, 'Выбери папку', '')
@@ -58,6 +87,14 @@ class SortMain(QWidget):
         self.DataBase.commit()
         self.DataBase.close()
 
+    def addFilesToDB(self, name, fromLoc, toLoc):
+        self.OpenDataBase()
+        date = str(datetime.datetime.now()).split('.')[0].split(':')[0]
+        BaseInfo = f'INSERT INTO Files(FileName, NowLocation, FromLocation, SortedDate) VALUES ("{name}", '
+        BaseInfo += f'"{toLoc}", "{fromLoc}", "{date}")'
+        self.curs.execute(BaseInfo).fetchall()
+        self.CloseDataBase()
+
     def LenghFormat(self):
         self.OpenDataBase()
         self.LikeDataBase = 'SELECT Format FROM FormatFile'
@@ -71,6 +108,7 @@ class SortMain(QWidget):
 
     def AllOrSelect(self):
         self.isAll = not self.isAll
+        print(self.isAll)
         if self.isAll:
             self.AllOrSelectButton.setText('̲В̲с̲е̲ ̲"̲.̲"/ Выбрать форматы')
             self.SelectFormatButton.setEnabled(False)
@@ -78,12 +116,30 @@ class SortMain(QWidget):
             self.AllOrSelectButton.setText('Все "." / ̲В̲ы̲б̲р̲а̲т̲ь̲ ф̲о̲р̲м̲а̲т̲ы̲')
             self.SelectFormatButton.setEnabled(True)
 
+    def CopyTo(self, data, file):
+        if ' '.join(data) not in os.listdir(self.ToPath):
+            os.mkdir(self.ToPath + '/' + ' '.join(data))
+        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+        self.addFilesToDB(file.split('/')[-1], file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+
+    def MoveTo(self, data, file):
+        if ' '.join(data) not in os.listdir(self.ToPath):
+            os.mkdir(self.ToPath + '/' + ' '.join(data))
+        shutil.move(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+        self.addFilesToDB(file.split('/')[-1], file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+
     def Sort(self):
         self.DB = 'bin/DataBase/BD.sqlite'
         os.walk(self.ToPath)
         if self.SelectFileLineEdit_2.text() == '':
-            with open('bin/DefaultPath.txt', mode='rt') as f:
-                self.ToPath = f.readlines()[0]
+            try:
+                with open('bin/DefaultPath.txt', mode='rt') as f:
+                    self.ToPath = f.readlines()[0]
+            except IndexError:
+                self.ResultLineEdit.setText('Некорректно введеный путь. Проверьте нижнее поле для ввода')
+            except FileNotFoundError:
+                with open('bin/DefaultPath.txt', mode='a') as f:
+                    self.ResultLineEdit.setText('Пересоздан корневой файл. Запустите сортировку еще раз')
         else:
             self.ToPath = self.SelectFileLineEdit_2.text()
         try:
@@ -99,35 +155,22 @@ class SortMain(QWidget):
                                 for i in os.listdir(file):
                                     files1.append(file + '/' + i)
                             else:
+                                Day = time.ctime(os.path.getctime(file)).split()[:2]
+                                Day = reversed(Day)
+                                if self.TypeSort.currentText() == 'Год/Месяц/День/Час':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
+                                            ' '.join(
+                                                Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]}) час:',
+                                            time.ctime(os.path.getctime(file)).split()[3].split(':')[0]]
+                                elif self.TypeSort.currentText() == 'Год/Месяц/День':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
+                                            ' '.join(Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]})']
+                                elif self.TypeSort.currentText() == 'Год/Месяц':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,', Day[1]]
+                                else:
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1]]
                                 if self.isAll:
-                                    if self.TypeSort.currentText() == 'Год/Месяц/День/Час':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        Day = reversed(Day)
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
-                                                ' '.join(Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]}) час:',
-                                                time.ctime(os.path.getctime(file)).split()[3].split(':')[0]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    elif self.TypeSort.currentText() == 'Год/Месяц/День':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        Day = reversed(Day)
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
-                                                ' '.join(Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]})']
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    elif self.TypeSort.currentText() == 'Год/Месяц':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,', Day[1]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    else:
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+                                    self.CopyTo(data, file)
                                 else:
                                     self.OpenDataBase()
                                     self.LikeDataBase = 'SELECT Format FROM FormatFile WHERE Chek = 1'
@@ -135,9 +178,11 @@ class SortMain(QWidget):
                                     for i in self.curs.execute(self.LikeDataBase).fetchall():
                                         LikeFormat2.append(i[0])
                                     if file.split('.')[1] in LikeFormat2:
-                                        shutil.copy(file, self.ToPath + '/' + file.split('/')[-1])
-                                    self.CloseDataBase()
-
+                                        if ' '.join(data) not in os.listdir(self.ToPath):
+                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
+                                        shutil.copy(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+                                        self.addFilesToDB(file.split('/')[-1], file,
+                                                          self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
                     elif self.RemakeFileBox.isChecked():
                         self.ResultLineEdit.setText('Успешно!')
                         files1 = os.listdir(self.ToFile)
@@ -148,35 +193,23 @@ class SortMain(QWidget):
                                 for i in os.listdir(file):
                                     files1.append(file + '/' + i)
                             else:
+                                Day = time.ctime(os.path.getctime(file)).split()[:2]
+                                Day = reversed(Day)
+                                if self.TypeSort.currentText() == 'Год/Месяц/День/Час':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
+                                            ' '.join(
+                                                Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]}) час:',
+                                            time.ctime(os.path.getctime(file)).split()[3].split(':')[0]]
+                                elif self.TypeSort.currentText() == 'Год/Месяц/День':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,',
+                                            ' '.join(Day) + f'(день: {time.ctime(os.path.getctime(file)).split()[2]})']
+                                elif self.TypeSort.currentText() == 'Год/Месяц':
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1], '- год,', Day[1]]
+                                else:
+                                    data = [time.ctime(os.path.getctime(file)).split()[-1]]
+
                                 if self.isAll:
-                                    if self.TypeSort.currentText() == 'Год/Месяц/День/Час':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        Day = reversed(Day)
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1],
-                                                ' '.join(Day) + f'(числа: {time.ctime(os.path.getctime(file)).split()[2]})',
-                                                time.ctime(os.path.getctime(file)).split()[3].split(':')[0]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.move(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    elif self.TypeSort.currentText() == 'Год/Месяц/День':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        Day = reversed(Day)
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1],
-                                                ' '.join(Day) + f'(числа: {time.ctime(os.path.getctime(file)).split()[2]})']
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.move(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    elif self.TypeSort.currentText() == 'Год/Месяц':
-                                        Day = time.ctime(os.path.getctime(file)).split()[:2]
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1], Day[1]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.move(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
-                                    else:
-                                        data = [time.ctime(os.path.getctime(file)).split()[-1]]
-                                        if ' '.join(data) not in os.listdir(self.ToPath):
-                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
-                                        shutil.move(file, self.ToPath + '/' + ' '.join(data) + '/' + file.split('/')[-1])
+                                    self.MoveTo(data, file)
                                 else:
                                     self.OpenDataBase()
                                     self.LikeDataBase = 'SELECT Format FROM FormatFile WHERE Chek = 1'
@@ -184,10 +217,11 @@ class SortMain(QWidget):
                                     for i in self.curs.execute(self.LikeDataBase).fetchall():
                                         LikeFormat2.append(i[0])
                                     if file.split('.')[1] in LikeFormat2:
-                                        shutil.copy(file, self.ToPath + '/' + file.split('/')[-1])
-                                    self.CloseDataBase()
-
-
+                                        if ' '.join(data) not in os.listdir(self.ToPath):
+                                            os.mkdir(self.ToPath + '/' + ' '.join(data))
+                                        shutil.move(file, self.ToPath + '/' + file.split('/')[-1])
+                                        self.addFilesToDB(file.split('/')[-1], file,
+                                                          self.ToPath + '/' + file.split('/')[-1])
                 else:
                     self.Error = ErrorWindow(101)
                     self.Error.show()
